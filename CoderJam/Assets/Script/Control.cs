108d80815b967309b4042c2a4f12df39fac1c779
Dawmuc <vincentpettineo@yahoo.fr>
2020-04-14 11:28:36 +0200

    Turrets + controler manager

---
+++ CoderJam/Assets/Script/Control.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public enum ControlType
+{
+	Turret,
+	Projectile
+}
+
+public class Control : MonoBehaviour
+{
+	public ControlType ControlMode { get; set; }
+	public Turret[] Turrets { get; set; }
+	public int CurrentTurretId { get; private set; }
+	public GameObject Projectile { get; set; }
+
+	public static Control instance;
+	
+	private void Awake()
+	{
+		instance = this;
+		
+		Turrets = FindObjectsOfType<Turret>();
+		IndexAllTurrets();
+
+		CurrentTurretId = 1;
+		Turrets[CurrentTurretId - 1].setHighLight(true);
+
+		ControlMode = ControlType.Turret;
+	}
+
+	private void Update()
+	{
+		if (ControlMode == ControlType.Projectile)
+		{
+
+		}
+		else if (ControlMode == ControlType.Turret)
+		{
+			if (Input.GetKeyDown(KeyCode.N))
+			{
+				NextTurret();
+			}
+		}
+	}
+
+	private void NextTurret()
+	{
+		Turrets[CurrentTurretId - 1].setHighLight(false);
+
+		CurrentTurretId++;
+		if (CurrentTurretId > Turrets.Length)
+			CurrentTurretId = 1;
+
+		Turrets[CurrentTurretId - 1].setHighLight(true);
+
+	}
+
+	private void IndexAllTurrets()
+	{
+		for (int i = 0; i < Turrets.Length; i++)
+		{
+			Turrets[i].ID = i + 1;
+		}
+	}
+}
---
+++ CoderJam/Assets/Script/Turret.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+	public int ID { get; set; }
+	[SerializeField] private float AngleSpeed = 1;
+	private float AngleRot = 0;
+	[SerializeField] private GameObject prefab;
+	[SerializeField] private Transform spawn;
+	[SerializeField] private float projectilSpeed = 10.0f;
+	[SerializeField] private GameObject highlight;
+
+	private void Update()
+	{
+		if (ID == Control.instance.CurrentTurretId && Control.instance.ControlMode == ControlType.Turret)
+		{
+			AdjustTurretAngle();
+
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				Fire();
+			}
+		}
+	}
+
+	private void Fire()
+	{
+		GameObject g = Instantiate(prefab, spawn.position, transform.rotation);
+		g.GetComponent<Rigidbody2D>().velocity = (g.transform.position - transform.parent.position).normalized * projectilSpeed;
+		Control.instance.Projectile = g;
+	}
+
+	private void AdjustTurretAngle()
+	{
+		AngleRot -= Input.GetAxis("Horizontal");
+
+		if (AngleRot < 0)
+			AngleRot = 359;
+		else if (AngleRot > 359)
+			AngleRot = 0;
+
+		transform.rotation = Quaternion.Euler(0, 0, AngleRot);
+	}
+
+	public void setHighLight(bool b)
+	{
+		if (b)
+			highlight.SetActive(true);
+		else
+			highlight.SetActive(false);
+	}
+}
